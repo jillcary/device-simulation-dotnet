2678cd9eb34d07457625cdca87c991bd863e0177
Jill Bender <jbend@microsoft.com>
2018-02-13 10:05:53 -0800

    fix spelling error

--- Services/data/devicemodels/scripts/turnstile-01-state.js
+++ Services/data/devicemodels/scripts/turnstile-01-state.js
@@ -27,17 +27,6 @@ function restoreState(previousState) {
 }
 
 /**
- * Simple formula generating a random value around the average
- * in between min and max
- */
-function vary(avg, percentage, min, max) {
-    var value =  avg * (1 + ((percentage / 100) * (2 * Math.random() - 1)));
-    value = Math.max(value, min);
-    value = Math.min(value, max);
-    return value;
-}
-
-/**
  * Entry point function called by the simulation engine.
  *
  * @param context        The context contains current time, device model and id
