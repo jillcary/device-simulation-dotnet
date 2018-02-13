442dbe364575a2448f4784a17ef9ad301eb1045c
Jill Bender <jbend@microsoft.com>
2018-02-13 14:12:47 -0800

    add commetns to clean up script & copy files to output directory

--- scripts/clean-up.cmd
+++ scripts/clean-up.cmd
@@ -15,20 +15,28 @@ echo Removing temporary folders and files...
 cd %APP_HOME%
 IF %ERRORLEVEL% NEQ 0 GOTO FAIL
 
+echo Removing \packages...
 rmdir /s /q .\packages
+
+echo Removing \target...
 rmdir /s /q .\target
+
+echo Removing \out...
 rmdir /s /q .\out
 
+echo Removing files in Services folder...
 rmdir /s /q .\Services\bin
 rmdir /s /q .\Services\obj
 rmdir /s /q .\Services.Test\bin
 rmdir /s /q .\Services.Test\obj
 
+echo Removing files in WebService folder...
 rmdir /s /q .\WebService\bin
 rmdir /s /q .\WebService\obj
 rmdir /s /q .\WebService.Test\bin
 rmdir /s /q .\WebService.Test\obj
 
+echo Removing files in SimulationAgent folder...
 rmdir /s /q .\SimulationAgent\bin
 rmdir /s /q .\SimulationAgent\obj
 rmdir /s /q .\SimulationAgent.Test\bin
